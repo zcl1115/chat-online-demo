6d231c95c7ff7b70f8e6185f413a41d9c2a6e736
michaelyjpcs <64138680+michaelyjpcs@users.noreply.github.com>
2020-04-28 21:51:26 +0800

    Add files via upload

--- server/routes/setting.js
+++ server/routes/setting.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const connection = require("../db/sql");
 const multiparty = require('multiparty');
 const fs = require('fs');
 
@@ -8,24 +7,12 @@ const LogoSavePathBase = "./UserLogos/";
 //const LogoSavePathBase = "E://UserLogos/";
 
 router.post('/GetPersonalInfo', (req, res) => {
-  var SQLString = 'select * from user where account = ?'
+  let InputData = {
+    UserID: req.body.UserID
+  };
 
-  connection.query(SQLString, [req.body.UserID], (err, results) => {
-    if (err) {
-      res.json({
-        Status: false
-      })
-    } else if (results.length == 1) {
-      res.json({
-        Status: true,
-        UserName: results[0].name,
-        UserIntroduction: results[0].personal_profile
-      })
-    } else {
-      res.json({
-        Status: false
-      })
-    }
+  db_helper.GetPersonalInfo(InputData, (ReturnData) => {
+    res.json(ReturnData);
   })
 })
 
@@ -52,67 +39,30 @@ router.post('/GetPersonalLogo', (req, res) => {
 })
 
 router.post('/SetPersonalInfo', (req, res) => {
-  var SQLString = 'update user set '
-  var SQLParam = []
-  var Mark = false
+  let InputData = {
+    UserID: req.body.UserID
+  };
 
-  console.log("test:", req.body);
-  if (req.body.NewLogoFileType) {
-    console.log("req.body.NewLogoFile: ", req.body.NewLogoFile);
+  if (req.body.NewName != undefined) {
+    InputData.NewName = req.body.NewName;
+  }
+  if (req.body.NewIntroduction != undefined) {
+    InputData.NewIntroduction = req.body.NewIntroduction;
   }
 
-  if (req.body.NewName != null) {
-    SQLString += 'name = ?'
-    SQLParam.push(req.body.NewName)
-    Mark = true
-  }
-  if (req.body.NewIntroduction != null) {
-    if (Mark) {
-      SQLString += ', '
-    }
-
-    SQLString += 'personal_profile = ?'
-    SQLParam.push(req.body.NewIntroduction)
-  }
-
-  SQLString += ' where account = ?'
-  SQLParam.push(req.body.UserID)
-
-  connection.query(SQLString, SQLParam, (err, results) => {
-    if (err) {
-      res.json({
-        Status: false
-      })
-    } else if (results.affectedRows == 1) {
-      res.json({
-        Status: true
-      })
-    } else {
-      res.json({
-        Status: false
-      })
-    }
+  db_helper.SetPersonalInfo(InputData, (ReturnData) => {
+    res.json(ReturnData);
   })
 })
 
 router.post('/SetPassword', (req, res) => {
-  var SQLString = 'update user set password = ? where account = ?'
-  var SQLParam = [req.body.NewPassword, req.body.UserID]
+  let InputData = {
+    UserID: req.body.UserID,
+    NewPassword: req.body.NewPassword
+  };
 
-  connection.query(SQLString, SQLParam, (err, results) => {
-    if (err) {
-      res.json({
-        Status: false
-      })
-    } else if (results.affectedRows == 1) {
-      res.json({
-        Status: true
-      })
-    } else {
-      res.json({
-        Status: false
-      })
-    }
+  db_helper.SetPassword(InputData, (ReturnData) => {
+    res.json(ReturnData);
   })
 })
 
@@ -153,31 +103,17 @@ router.post('/NewLogo', (req, res) => {
 })
 
 router.post('/SetLoginStatus', (req, res) => {
-  console.log("req.body:", req.body);
-  var LoginStatus = 1;
-  if(req.body.LoginStatus === "false") {
-    LoginStatus = 0;
+  let InputData = {
+    UserID: req.body.UserID,
+    LoginStatus: 1
+  };
+
+  if (req.body.LoginStatus === "false") {
+    InputData.LoginStatus = 0;
   }
 
-  var SQLString = 'update user set online_status = ? where account = ?';
-  var SQLParam = [LoginStatus, req.body.UserID];
-
-  console.log("SQLParam:", SQLParam);
-
-  connection.query(SQLString, SQLParam, (err, results) => {
-    if (err) {
-      res.json({
-        Status: false
-      })
-    } else if (results.affectedRows == 1) {
-      res.json({
-        Status: true
-      })
-    } else {
-      res.json({
-        Status: false
-      })
-    }
+  db_helper.SetLoginStatus(InputData, (ReturnData) => {
+    res.json(ReturnData);
   })
 })
 
