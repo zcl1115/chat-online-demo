5688c014146dea18a3dd19bfe1854cf0aaa3d2ab
zcl1115 <45005291+zcl1115@users.noreply.github.com>
2020-05-12 15:58:12 +0800

    增加了聊天记录的管理

---
+++ chat.js
@@ -0,0 +1,347 @@
+var moment = require('moment');
+var express = require('express');
+var router = express.Router();
+var http = require('http');
+var fs = require('fs');
+const readline = require('readline');
+const _path = require('path');
+var server = http.createServer(function (req, res) {
+}).listen(3001);//创建http服务
+
+var LogoSavePathBase = "./UserLogos/";
+
+console.log('Server running ');
+var io = require('socket.io').listen(server);
+
+var online_users = {}; // records online users'socket
+io.on('connection', function (socket) {
+  console.log("A user connected");
+  var account = null;
+
+  // register account with socket
+  socket.on('register', function (str) {
+    console.log(str.account + "login");
+    account = str.account;
+    online_users[str.account] = socket;
+    db_helper.setOnline(str.account);
+    // load friends' avatar
+    db_helper.getImgPath(account, function (img_path) {
+      socket.emit("login", { 'account': account, 'img_path': img_path });
+    })
+    // load offline messages
+    db_helper.getOfflineMessage(str.account, function (messages) {
+      for (var i = 0; i < messages.length; i++) {
+        socket.emit('message', { 'from': messages[i].account_from, 'to': messages[i].account_to, 'type': messages[i].type, 'content': messages[i].message, 'time': messages[i].time });
+      }
+      // clear stored offline messages
+      db_helper.clearOfflineMessage(account);
+    })
+
+
+  });
+
+  // sendMessage event
+  socket.on('sendMessage', function (str) {
+    let time = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
+    console.log("(sendMessage) account: " + account + ' to ' + str.to + ' message: ' + str.message + 'time: ' + time);
+
+    let path;
+    //存放消息记录
+    if (!fs.existsSync('./ChatHistory/' + account + " " + str.to) &&
+        !fs.existsSync('./ChatHistory/' + str.to + " " + account))
+    {
+      fs.mkdirSync('./ChatHistory/' + account + " " + str.to);
+    }
+
+    if (fs.existsSync('./ChatHistory/' + account + " " + str.to))
+    {
+      path = './ChatHistory/' + account + " " + str.to;
+    }
+
+    if (fs.existsSync('./ChatHistory/' + str.to + " " + account))
+    {
+      path = './ChatHistory/' + str.to + " " + account;
+    }
+
+    let chat_date = moment(Date.now()).format('YYYY-MM-DD');
+    let chat_time = moment(Date.now()).format('HH:mm:ss');
+    //文件夹命名为用户a账号 用户b账号,则三个标志位分别为a对该信息的删除情况,b对该信息的删除情况,该信息是消息还是文件
+    let message = "0 " + "0 " + "0 " + chat_time + " " + account + " " + str.to + " " + str.message + '\n';
+    fs.writeFile(path + '/' + chat_date + '.txt', message, { 'flag': 'a' }, function (error) {
+      if (error) {
+        console.log('存储失败')
+      } else {
+        console.log('存储成功')
+      }
+    });
+
+    // Online
+    if (online_users[str.to] !== undefined) {
+      console.log("online");
+      online_users[str.to].emit('message', { 'from': account, 'to': str.to, 'type': 0, 'content': str.message, 'time': time });
+    } else {
+      // Offline operation
+      console.log("offline");
+      db_helper.storeOfflineMessage(account, str.to, 0, str.message, time);
+    }
+
+    // update mutual recent contact list
+    db_helper.updateRecentList(account, time, str.to, null);
+    db_helper.updateRecentList(str.to, time, account, null);
+  });
+
+  // sendFile event
+  socket.on('sendFile', function (str) {
+    let time = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
+    console.log("(sendFile) account: " + account + ' to ' + str.to + 'time: ' + time);
+
+    if (!fs.existsSync('./files/' + account)) fs.mkdirSync('./files/' + account);
+    if (!fs.existsSync('./files/' + account + '/' + str.to)) fs.mkdirSync('./files/' + account + '/' + str.to);
+    // store file
+    fs.writeFile('./files/' + account + '/' + str.to + '/' + str.file_name, str.arraybuffer, function (error) {
+      if (error) {
+        console.log(error);
+        console.log('存储失败')
+      } else {
+        console.log('存储成功')
+      }
+    });
+
+    // Online
+    if (online_users[str.to] !== undefined) {
+      console.log("online");
+      online_users[str.to].emit('message', { 'from': account, 'to': str.to, 'type': 1, 'content': str.file_name, 'time': time });
+    } else {
+      // Offline operation
+      console.log("offline");
+      db_helper.storeOfflineMessage(account, str.to, 1, str.file_name, time);
+    }
+
+    // update mutual recent contact list
+    db_helper.updateRecentList(account, time, str.to, null);
+    db_helper.updateRecentList(str.to, time, account, null);
+
+  });
+
+  // getFile event
+  socket.on('getFile', function (str) {
+    console.log("getFile called");
+    fs.readFile("./files/" + str.from + "/" + str.to + "/" + str.file_name, (err, data) => {
+      if (err) throw err;
+      online_users[account].emit('getFile', { 'from': str.from, 'to': str.from, 'arraybuffer': data.buffer, 'file_name': str.file_name });
+    });
+  });
+
+  // disconnect event
+  socket.on('disconnect', function () {
+    if (account != null) {
+      db_helper.setOffline(account, function () {
+        online_users[account] = undefined;
+        console.log("(disconnect) account: " + account);
+      });
+    } else {
+      console.log("(disconnect) account: undefined");
+    }
+  });
+});
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/get_recent_list', function (req, res, next) {
+  db_helper.getRecentList(req.body.account, function (results) {
+    let user;
+    for (user in results) {
+      var FilePath = LogoSavePathBase + results[user].contact;
+      let data = fs.readFileSync(FilePath, (err, data) => {
+      });
+      results[user].img_path = data;
+    }
+    res.json(results);
+  });
+});
+
+router.post('/search', function (req, res, next) {
+  var flag_pos;
+  var account = req.body.account;
+  var contact = req.body.contact;
+  var date = req.body.date;
+  let path;
+  //寻找存放消息记录路径
+
+  var list_map = new Array();
+
+  if (fs.existsSync('./ChatHistory/' + account + " " + contact))
+  {
+    path = './ChatHistory/' + account + " " + contact;
+    flag_pos = 0;
+  }
+
+  if (fs.existsSync('./ChatHistory/' + contact + " " + account))
+  {
+    path = './ChatHistory/' + contact + " " + account;
+    flag_pos = 1;
+  }
+
+  let filepath = _path.join(path, date + '.txt');
+  let input = fs.createReadStream(filepath);
+  input.on('error',function(err){
+    res.json({
+      status: false
+    })
+  });
+  const rl = readline.createInterface({
+    input: input
+  });
+  rl.on('line', (line) => {
+    var b = ((line.toString()).split(" "));
+    if (b[flag_pos]==="0"){
+      list_map.push({
+        type: b[2],
+        time: date + " " + b[3],
+        from: b[4],
+        to: b[5],
+        message: b[6]
+      })
+    }
+  });
+  rl.on('close', (line) => {
+    res.json({
+      status: true,
+      chat_history_list: list_map
+    })
+  });
+
+});
+
+router.post('/search_key', function (req, res, next) {
+  var flag_pos;
+  var account = req.body.account;
+  var contact = req.body.contact;
+  var start_date = req.body.start_date;
+  var end_date = req.body.end_date;
+  var key_word = req.body.key_word;
+  var date_list = new Array();
+  var end = new Date(end_date).getTime();
+  for (var start = new Date(start_date).getTime();start <= end; start += 3600 * 1000 * 24){
+    let time = moment(new Date(start)).format('YYYY-MM-DD');
+    date_list.push(time);
+  }
+  let path;
+  //寻找存放消息记录路径
+
+  var list_map = new Array();
+
+  if (fs.existsSync('./ChatHistory/' + account + " " + contact))
+  {
+    path = './ChatHistory/' + account + " " + contact;
+    flag_pos = 0;
+  }
+
+  if (fs.existsSync('./ChatHistory/' + contact + " " + account))
+  {
+    path = './ChatHistory/' + contact + " " + account;
+    flag_pos = 1;
+  }
+  //查询结果中是否有错误以及是否全部读取完成
+  var error_flag = false;
+  var read_sum = date_list.length;
+  for (let date in date_list){
+    let filepath = _path.join(path, date_list[date] + '.txt');
+    let input = fs.createReadStream(filepath);
+    input.on('error',function(err){
+      error_flag = true;
+      read_sum --;
+    });
+    const rl = readline.createInterface({
+      input: input
+    });
+    rl.on('line', (line) => {
+      var b = ((line.toString()).split(" "));
+      if (b[flag_pos]==="0"){
+        if (b[6].search(key_word)!==-1){
+          list_map.push({
+            type: b[2],
+            time: date_list[date] + " " + b[3],
+            from: b[4],
+            to: b[5],
+            message: b[6]
+          });
+        }
+      }
+    });
+    rl.on('close', (line) => {
+      read_sum --;
+      if (read_sum === 0){
+        if (error_flag){
+          res.json({
+            //status: false,
+            chat_history_list: list_map
+          })
+        }else{
+          res.json({
+            //status: true,
+            chat_history_list: list_map
+          })
+        }
+      }
+    });
+  }
+
+});
+
+router.post('/delete', function (req, res, next) {
+  var flag_pos;
+  var account = req.body.account;
+  var contact = req.body.contact;
+  var date = req.body.date;
+  let path;
+  //寻找存放消息记录路径
+  var temp = ((date.toString()).split(" "));
+  date = temp[0];
+  var time = temp[1];
+  if (fs.existsSync('./ChatHistory/' + account + " " + contact))
+  {
+    path = './ChatHistory/' + account + " " + contact;
+    flag_pos = 0;
+  }
+
+  if (fs.existsSync('./ChatHistory/' + contact + " " + account))
+  {
+    path = './ChatHistory/' + contact + " " + account;
+    flag_pos = 1;
+  }
+
+  var fWriteName = path + '/temp.txt';
+  let filepath = _path.join(path, date + '.txt');
+  let input = fs.createReadStream(filepath);
+  var fWrite = fs.createWriteStream(fWriteName);
+  input.on('error',function(err){
+    res.json({
+      status: false
+    })
+  });
+  const rl = readline.createInterface({
+    input: input
+  });
+  rl.on('line', (line) => {
+    var b = ((line.toString()).split(" "));
+    if (b[flag_pos]==="0" && b[3]===time){
+      b[flag_pos]="1";
+      line = b[0] + " " + b[1] + " " + b[2] + " " + b[3] + " " + b[4] + " " + b[5] + " " + b[6];
+    }
+    fWrite.write(line.toString() + '\n');
+  });
+  rl.on('close', (line) => {
+    fs.unlinkSync(path + '/' + date + '.txt');
+    fs.rename(fWriteName,path + '/' + date + '.txt',function(err){
+    });
+    res.json({
+      status: true,
+    })
+  });
+
+});
+module.exports = router;
---
+++ chatting.vue
@@ -0,0 +1,610 @@
+<template>
+  <el-container :class="{el_dark: IsDarkMode}">
+    <el-aside class="MiddleAside" :class="{MiddleAsideDark: IsDarkMode}" width="300px">
+      <div class="search_area">
+        <el-input
+          :class="{dark_search: IsDarkMode}"
+          placeholder="搜索最近联系人"
+          v-model="search_name"
+          suffix-icon="el-icon-search"
+        ></el-input>
+      </div>
+      <div class="recent_contacts">
+        <ul
+          v-for="(people,index) in filter_recent_contacts"
+          v-bind:key="index"
+          class="contact"
+          :class="{contact_dark: IsDarkMode, addclassdark: contact_account === people.contact&&IsDarkMode,addclass: contact_account === people.contact}"
+          v-on:click="select_contact(people.name,people.contact,people.img_path)"
+        >
+          <img :src="people.img_path" class="round_icon" />
+          <a class="contact_name">{{people.name}}</a>
+          <img
+            src="../assets/point.png"
+            class="notice"
+            v-if="unread_contacts.indexOf(people.contact) !== -1 && contact_account !== people.contact"
+          />
+        </ul>
+      </div>
+    </el-aside>
+    <div class="chat_area" v-if="isShow" :class="{chat_area_dark: IsDarkMode}">
+      <div class="friend_name" :class="{friend_name_dark: IsDarkMode}">
+        {{contacts}}
+        <!--          {{contact_account}}-->
+      </div>
+      <!--eslint-disable-next-line-->
+      <div class="message_show" v-if="!history_show">
+        <div class="message_box" v-for="(message, index) in messages" :key="index">
+          <div v-if="message.to === contact_account && message.from === account">
+            <div class="btalk">
+              <div class="avatar_box">
+                <img :src="img_path" class="contactor_avatar" />
+              </div>
+              <span class="message" v-if="message.type === 0">{{message.content}}</span>
+              <span class="message" v-if="message.type === 1">
+                <a class="file_name">{{message.content}}</a>
+                <img class="file_icon" src="../assets/download.png" v-on:click="download(message)" />
+              </span>
+              <br />
+              <br />
+              <br />
+              <br />
+            </div>
+          </div>
+          <div v-if="message.from === contact_account && message.to === account">
+            <div class="atalk">
+              <div class="avatar_box">
+                <img :src="contact_img" class="contactor_avatar" />
+              </div>
+              <span class="message" v-if="message.type === 0">{{message.content}}</span>
+              <span class="message" v-if="message.type === 1">
+                <img class="file_icon" src="../assets/download.png" v-on:click="download(message)" />
+                <a class="file_name">{{message.content}}</a>
+              </span>
+              <br />
+              <br />
+              <br />
+              <br />
+            </div>
+          </div>
+        </div>
+      </div>
+      <component
+              :is="chat_history_show"
+              :IsDarkMode="IsDarkMode"
+              :contact="contact_account"
+              :img_path="contact_img"
+              :account="account"
+      ></component>
+      <div class="input_message">
+        <div class="function_area" style>
+          <img
+            class="icon"
+            src="../assets/emoji.svg"
+            v-on:click="emoji_picker_isShow = !emoji_picker_isShow"
+          />
+          <img
+            class="icon"
+            src="../assets/upload.svg"
+            onclick="document.getElementById('upload_clicked').click();"
+          />
+          <img
+                  class="icon"
+                  src="../assets/history.svg"
+                  v-on:click="show_history()"
+          />
+          <input type="file" class="upload" id="upload_clicked" v-show="false" @change="uploadFile" />
+          <emoji_picker @emoji_clicked="emoji_clicked" :inputData="emoji_picker_isShow"></emoji_picker>
+        </div>
+        <div class="input_area">
+          <textarea class="content" :class="{content_dark: IsDarkMode}" id="message" type="text" />
+        </div>
+        <div class="send_area">
+          <el-button
+            type="primary"
+            v-on:click="send_message()"
+            style="background: rgb(80,80,160);border:rgb(80,80,160)"
+          >
+            发送
+            <i class="el-icon-s-promotion el-icon--right"></i>
+          </el-button>
+        </div>
+      </div>
+    </div>
+  </el-container>
+</template>
+
+<script>
+import { getCookie } from "../components/cookieUtil";
+import emoji_picker from "../components/emoji_picker";
+import FileSaver from "file-saver";
+import chat_history from "./chat_history.vue";
+
+export default {
+  components: {
+    emoji_picker,
+    chat_history
+  },
+  data() {
+    return {
+      account: "",
+      img_path: "",
+      logoURL: require("../assets/logo.png"),
+      contacts: "",
+      contact_account: "",
+      recent_contacts_list: [],
+      isShow: false,
+      contact_img: "",
+      messages: [],
+      unread_contacts: [],
+      emoji_picker_isShow: false,
+      file_selector_isShow: false,
+      cur_file: "",
+      fileData: "",
+      search_name: "",
+      history_show: false,
+      chat_history_show: ""
+    };
+  },
+  props: {
+    IsDarkMode: {
+      type: Boolean
+    },
+    selected_contact: {
+      type: String
+    }
+  },
+  created() {
+    this.account = getCookie("user_account");
+    this.init_recent_contacts();
+  },
+  mounted() {
+    this.sockets.subscribe("message", data => {
+      this.messages.push(data);
+      this.unread_contacts.push(data.from);
+
+      var i = this.get_index(data.from);
+      let temp = this.recent_contacts_list[i];
+      temp.chat_time = data.time;
+      this.$set(this.recent_contacts_list, i, temp);
+      this.recent_contacts_list.sort((a, b) => {
+        let atime = a.chat_time;
+        let btime = b.chat_time;
+        atime = atime.replace(/-/g, "/");
+        btime = btime.replace(/-/g, "/");
+        let a1 = new Date(atime).getTime();
+        let b1 = new Date(btime).getTime();
+        return b1 - a1;
+      });
+    });
+    this.sockets.subscribe("getFile", data => {
+      console.log("receive file");
+      var blob = new Blob([data.arraybuffer], {
+        type: "text/plain;charset=utf-8"
+      });
+      FileSaver.saveAs(blob, data.file_name);
+      // var blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
+      // FileSaver.saveAs(blob, "hello world.txt");
+    });
+    this.sockets.subscribe("login", data => {
+      console.log(data);
+    });
+  },
+  methods: {
+    select_contact(name, account, img_path) {
+      this.img_path = getCookie("UserLogoPath");
+      this.contacts = name;
+      this.contact_account = account;
+      this.isShow = true;
+      this.contact_img = img_path;
+      this.unread_contacts.splice(this.unread_contacts.indexOf(account), 1);
+    },
+    init_recent_contacts() {
+      var temp = getCookie("user_account");
+      this.axios
+        .post(
+          "api/chat/get_recent_list",
+          this.qs.stringify({
+            account: temp
+          })
+        )
+        .then(response => {
+          this.recent_contacts_list = response.data;
+          console.log(this.recent_contacts_list);
+          let i;
+          for (i in this.recent_contacts_list) {
+            var FileLength = this.recent_contacts_list[i].img_path.data.length;
+            var Array1 = new ArrayBuffer(FileLength);
+            var Array2 = new Uint8Array(Array1);
+
+            for (var j = 0; j < FileLength; j++) {
+              Array2[j] = this.recent_contacts_list[i].img_path.data[j];
+            }
+
+            var FileBlob = new Blob([Array2], { type: "" });
+            var UserLogoURL = URL.createObjectURL(FileBlob);
+            this.recent_contacts_list[i].img_path = UserLogoURL;
+          }
+
+          this.recent_contacts_list.sort((a, b) => {
+            let atime = a.chat_time;
+            let btime = b.chat_time;
+            atime = atime.replace(/-/g, "/");
+            btime = btime.replace(/-/g, "/");
+            let a1 = new Date(atime).getTime();
+            let b1 = new Date(btime).getTime();
+            return b1 - a1;
+          });
+
+          if (this.selected_contact){
+            let t;
+            for (t in this.recent_contacts_list) {
+              if (this.selected_contact===this.recent_contacts_list[t].contact){
+                this.img_path = getCookie("UserLogoPath");
+                this.contacts = this.recent_contacts_list[t].name;
+                this.contact_account = this.selected_contact;
+                this.isShow = true;
+                this.contact_img = this.recent_contacts_list[t].img_path;
+                this.unread_contacts.splice(this.unread_contacts.indexOf(this.selected_contact), 1);
+                break;
+              }
+            }
+          }
+        });
+    },
+    send_message() {
+      // console.log("send called");
+      var message = document.getElementById("message").value;
+      this.$socket.emit("sendMessage", {
+        to: this.contact_account,
+        message: message
+      });
+      this.messages.push({
+        from: getCookie("user_account"),
+        to: this.contact_account,
+        type: 0,
+        content: message
+      });
+      document.getElementById("message").value = "";
+      // console.log(this.messages);
+    },
+    get_index(account) {
+      var i = 0;
+      for (; i < this.recent_contacts_list.length; i++) {
+        if (this.recent_contacts_list[i].contact === account) {
+          return i;
+        }
+      }
+      return -1;
+    },
+    emoji_clicked(emoji) {
+      this.emoji_picker_isShow = false;
+      document.getElementById("message").value += emoji;
+    },
+    uploadFile(e) {
+      this.cur_file = e.target.files[0];
+      if (this.cur_file === undefined) return;
+      const isLt1M = this.cur_file.size < 100 * 1024 * 1024;
+      if (!isLt1M) {
+        this.$message.error("上传文件大小不能超过 100MB!");
+        return;
+      }
+      var self = this;
+      let reader = new FileReader();
+      console.log(this.cur_file);
+      reader.onload = function() {
+        var arraybuffer = this.result;
+        self.$socket.emit("sendFile", {
+          to: self.contact_account,
+          file_name: self.cur_file.name,
+          lastmodfiyTimeDate: self.cur_file.lastmodfiyTimeDate,
+          arraybuffer: arraybuffer
+        });
+        self.messages.push({
+          from: getCookie("user_account"),
+          to: self.contact_account,
+          type: 1,
+          content: self.cur_file.name
+        });
+      };
+      reader.readAsArrayBuffer(this.cur_file);
+    },
+    download(message) {
+      console.log("download request called");
+      this.$socket.emit("getFile", {
+        from: message.from,
+        to: message.to,
+        file_name: message.content
+      });
+    },
+    show_history(){
+      if (this.history_show){
+        this.chat_history_show = "";
+      }else{
+        this.chat_history_show = "chat_history";
+      }
+      this.history_show = !this.history_show;
+    }
+  },
+  computed: {
+    filter_recent_contacts: function() {
+      var _this = this;
+      var new_items = [];
+      this.recent_contacts_list.map(function(item) {
+        if (
+          _this.search_name === "" ||
+          item.name.search(_this.search_name) !== -1
+        )
+          new_items.push(item);
+      });
+      return new_items;
+    }
+  }
+};
+</script>
+<style>
+.dark_search .el-input__inner {
+  background-color: rgb(0, 0, 0);
+  color: rgb(240, 240, 240);
+}
+</style>
+<style  lang="less" scoped>
+.el-container {
+  height: 100%;
+  margin: 0;
+}
+.el_dark {
+  background-color: rgb(0, 0, 0);
+}
+.MiddleAside {
+  background-color: rgb(247, 247, 247);
+  text-align: center;
+}
+
+.MiddleAsideDark {
+  background-color: rgb(0, 0, 0);
+}
+
+.search_area {
+  text-align: center;
+  width: 100%;
+  height: 15%;
+  line-height: 6;
+}
+.recent_contacts {
+  width: 100%;
+  height: 83%;
+  overflow: auto;
+  overflow-x: hidden;
+}
+
+.chat_area {
+  width: 75%;
+  height: 100%;
+  float: left;
+}
+.char_area_dark {
+  background-color: rgb(0, 0, 0);
+}
+
+.friend_name {
+  text-align: center;
+  width: 100%;
+  height: 6%;
+  box-shadow: 0px 2px 0px 0px #f2f2f2;
+}
+.friend_name_dark {
+  color: rgb(240, 240, 240);
+}
+.message_show {
+  width: 100%;
+  height: 67%;
+  overflow: auto;
+  overflow-x: hidden;
+}
+
+.input_message {
+  width: 100%;
+  height: 27%;
+  box-shadow: 0px -2px 0px 0px #f2f2f2;
+}
+.function_area {
+  width: 100%;
+  height: 15%;
+}
+.input_area {
+  margin-top: 2px;
+  margin-bottom: 2px;
+  width: 100%;
+  height: 50%;
+}
+.content {
+  border: none;
+  resize: none;
+  cursor: pointer;
+  padding-top: 5px;
+  font-family: "SimSun", "Times New Roman", Georgia, Serif, serif;
+  width: 100%;
+  height: 100%;
+  line-height: 10px;
+  font-size: 20px;
+}
+.content_dark {
+  background-color: rgb(0, 0, 0);
+  color: rgb(240, 240, 240);
+}
+.content:focus {
+  outline: none;
+}
+.send_area {
+  margin-top: 8px;
+  text-align: right;
+  float: right;
+  width: 100%;
+  height: 23%;
+}
+.round_icon {
+  width: 40px;
+  height: 40px;
+  display: flex;
+  border-radius: 50%;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  float: left;
+  margin-left: -30px;
+}
+.contact_name {
+  float: left;
+  width: 100px;
+  text-align: left;
+  margin-top: 12px;
+  margin-left: 20px;
+}
+.notice {
+  float: left;
+  margin-top: 18px;
+  width: 10px;
+  height: 10px;
+}
+
+.contact {
+  width: 100%;
+  height: 60px;
+}
+
+.contact_dark {
+  color: rgb(240, 240, 240);
+}
+
+.icon {
+  margin-left: 5px;
+  width: 25px;
+  height: 25px;
+}
+.inline-block {
+  display: inline-block;
+}
+.upload {
+  opacity: 0;
+}
+p {
+  margin-top: -8px;
+  margin-left: 30px;
+}
+.addclass {
+  background-color: white;
+}
+.addclassdark {
+  background-color: #1a1a1a;
+  color: rgb(250, 250, 250);
+}
+.message_box {
+  /*width: 50%;*/
+}
+
+.atalk {
+  overflow: hidden;
+  float: none;
+  margin-right: 50%;
+  width: 50%;
+  text-align: left;
+}
+.atalk .message {
+  float: left;
+  padding: 5px 0px 5px 10px;
+  margin-left: 5%;
+  width: 50%;
+  display: inline-block;
+  *display: inline;
+  *zoom: 1;
+  text-align: justify;
+  text-justify: newspaper;
+  word-break: break-all;
+  background: #f8f6f8;
+  color: #000000;
+  border-radius: 10px;
+}
+.atalk .file {
+  float: left;
+  padding: 5px 10px 5px 0px;
+  margin-right: 5%;
+  width: 50%;
+  display: inline-block;
+  *display: inline;
+  *zoom: 1;
+  text-align: right;
+  text-justify: newspaper;
+  word-break: break-all;
+  background: #50519f;
+  color: #ffffff;
+  border-radius: 10px;
+}
+.atalk .file_icon {
+  float: left;
+  margin-right: 10px;
+  width: 50px;
+  height: 50px;
+}
+.atalk .avatar_box {
+  float: left;
+  margin-left: 5%;
+  width: 40px;
+  height: 40px;
+}
+.atalk .contactor_avatar {
+  float: left;
+  width: 40px;
+  height: 40px;
+}
+.atalk .file_name {
+  position: relative;
+  float: left;
+  top: 10px;
+}
+.btalk {
+  overflow: hidden;
+  float: none;
+  margin-left: 50%;
+  width: 50%;
+  text-align: right;
+}
+.btalk .message {
+  float: right;
+  padding: 5px 10px 5px 0px;
+  margin-right: 5%;
+  width: 50%;
+  display: inline-block;
+  *display: inline;
+  *zoom: 1;
+  text-align: right;
+  text-justify: newspaper;
+  word-break: break-all;
+  background: #50519f;
+  color: #ffffff;
+  border-radius: 10px;
+}
+
+.btalk .file_icon {
+  float: right;
+  margin-right: 10px;
+  width: 40px;
+  height: 40px;
+}
+.btalk .file_name {
+  position: relative;
+  float: right;
+  top: 10px;
+}
+.btalk .avatar_box {
+  float: right;
+  margin-right: 5%;
+  width: 40px;
+  height: 40px;
+}
+.btalk .contactor_avatar {
+  float: right;
+  width: 40px;
+  height: 40px;
+}
+</style>
