6cc3ea5dfe2c4dd45a47eaa38722f878211afeea
silencew312 <56785491+silencew312@users.noreply.github.com>
2020-05-13 14:48:32 +0800

    新增添加好友

--- server/routes/friend.js
+++ server/routes/friend.js
@@ -22,6 +22,42 @@ router.post('/update_recent_contacts', function(req, res, next){
     });
 
 });
+router.post('/get_user', function(req, res, next){
+    db_helper.getUser(req.body.account, function (results) {
+        let user;
+        for (user in results) {
+            console.log(results[user]);
+            var FilePath = LogoSavePathBase +results[user].account;
+            let data = fs.readFileSync(FilePath, (err, data) => {
+            });
+            results[user].img_path = data;
+        }
+        res.json(results);
+    });
 
+});
+router.post('/is_contact', function(req, res, next){
+    db_helper.isContact(req.body.user_account,req.body.contact_account, function (status) {
+        if(status===0){
+            res.json({
+                IsContact:true
+            });
+            return;
+        }
+        else{
+            res.json({
+                IsContact:false
+            });
+            return;
+        }
+    });
+
+});
+router.post('/add_contact', function(req, res, next){
+    db_helper.addContact(req.body.user_account,req.body.contact_account,req.body.name, function (results) {
+        res.json(results);
+    });
+
+});
 
 module.exports = router;
