57ade867a0c2e1281b1b2d35464f976cb2f642ac
silencew312 <56785491+silencew312@users.noreply.github.com>
2020-05-19 01:46:11 +0800

    新增查找联系人

--- server/routes/friend.js
+++ server/routes/friend.js
@@ -16,6 +16,20 @@ router.post('/get_contacts', function(req, res, next){
     });
 
 });
+router.post('/get_contact', function(req, res, next){
+    db_helper.getContact(req.body.user_account, req.body.str,function (results) {
+        let user;
+        for (user in results) {
+            console.log(results[user]);
+            var FilePath = LogoSavePathBase + results[user].account;
+            let data = fs.readFileSync(FilePath, (err, data) => {
+            });
+            results[user].img_path = data;
+        }
+        res.json(results);
+    });
+
+});
 router.post('/update_recent_contacts', function(req, res, next){
     db_helper.updateRecentList(req.body.user_account,req.body.time,req.body.contact_account, function (results) {
         res.json(results);
